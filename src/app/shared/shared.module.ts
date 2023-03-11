ec47649186ff02b75e809fe87c692c18c5538956
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2023-03-12 00:04:19 +0100

    feat: add a footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { OrbsBackgroundComponent } from './components/orbs-background/orbs-background.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
-  declarations: [OrbsBackgroundComponent, NavbarComponent],
+  declarations: [OrbsBackgroundComponent, NavbarComponent, FooterComponent],
   imports: [CommonModule, RouterModule],
-  exports: [OrbsBackgroundComponent, NavbarComponent],
+  exports: [OrbsBackgroundComponent, NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
